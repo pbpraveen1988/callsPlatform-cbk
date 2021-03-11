d53f714dbe464f93a21bd045ed6bf1f46cfb3901
PraveenKumar <pb.praveen@xymob.in>
2021-03-11 17:53:13 +0530

    Will create the callback worker.

---
+++ crons/callback/response.js
@@ -0,0 +1,197 @@
+const { ObjectID } = require('mongodb');
+const axios = require('axios');
+const moment = require('moment');
+const log4js = require('log4js');
+const logger = log4js.getLogger('Callback');
+const { RinglessDB } = require('../../global/constants');
+const { MongoClient } = require('mongodb');
+class RespHandler {
+  constructor() {
+    this._respTimerObj = null;
+    this._callbackTimerObj = null;
+    this.initDB();
+    this._callbackTimer();
+    // this._respTimer();
+    console.log('RESPONSE TIMER STARTED');
+
+  }
+
+  async initDB() {
+    const mongoUrl = 'mongodb://127.0.0.1:27017/RinglessVM';
+    const mongoDBName = 'RinglessVM';
+    this._conn = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, poolSize: 10, useUnifiedTopology: true });
+    this._db = this._conn.db(mongoDBName);
+    RinglessDB(this._db);
+  }
+
+
+  getResponseValues(dto) {
+    const _newValue = {};
+    _newValue.uuid = dto.uuid;
+    _newValue.carrier = dto.Carrier;
+    _newValue.status = dto.isError ? 'failed' : 'success';
+    _newValue.timestamp = new Date();
+    _newValue.number = dto.PhoneTo;
+    _newValue.drop_callerid = dto.PhoneFrom;
+    _newValue.error_message = dto.ErrorMessage;
+    if (_newValue.error_message != undefined && _newValue.error_message != '') {
+      _newValue.status = 'failed';
+    }
+    _newValue.drop_method = dto.drop_method;
+    _newValue.drop_result = dto.CallStatus;
+    if (dto.CallStatus === 'completed') {
+      _newValue.drop_result = 'success';
+    } else {
+      _newValue.drop_result = 'failed';
+    }
+
+    // if (_newValue.status === 'failed' && _newValue.carrier == 'UNSUPPORTED CARRIER') {
+    //   _newValue.drop_message = 'VM Failure';
+    // }
+
+    _newValue.number_type = dto.number_type;
+    _newValue.drop_timestamp = dto.DateAddedToQueue;
+    _newValue.external_id1 = dto.external_id1;
+    _newValue.external_id2 = dto.external_id2;
+    _newValue.external_id3 = dto.external_id3;
+    _newValue.external_id4 = dto.external_id4;
+    _newValue.number_type = dto.number_type;
+    return _newValue;
+  }
+
+  _callbackTimer() {
+
+    console.log('CALLBACK TIMER')
+    if (this._callbackTimerObj) { return; }
+    this._callbackTimerObj = setTimeout(async () => {
+      try {
+        const db = RinglessDB();
+        if (!db) {
+          this._callbackTimerObj = null;
+          this._callbackTimer();
+          return;
+        }
+        const tmrResps = await db.collection('responses').find({ SentToCallback: { $in: [null, false] }, callback_url: { $nin: [null] } }).limit(500).toArray();
+        console.log('CALLBACK Records Count', tmrResps && tmrResps.length);
+        const tmrArr = [];
+        if (tmrResps.length) {
+          for (const tmr of tmrResps) {
+            if (tmr.callback_url) {
+              const pro = new Promise(async (resolve, reject) => {
+                try {
+                  const dto = tmr;
+                  try {
+                    logger.addContext('campaignId', 'CALLBACK_' + dto.CampaignId);
+                  } catch (ex) {
+
+                  }
+                  const _callbackResponse = this.getResponseValues(dto);
+                  const dtoCopy = Object.assign({}, dto);
+                  delete dtoCopy._id;
+                  delete dto._id;
+                  delete dto.CallId;
+                  delete dto.SentToCallback;
+                  delete dto.DateCreated;
+                  delete dto.StartDate;
+                  delete dto.EndDate;
+                  delete dto.Attempts;
+                  delete dto.TotalSeconds;
+                  const ___response = await axios.post(tmr.callback_url, _callbackResponse).catch(err => {
+                    console.error(err);
+                    logger.debug('ERROR ON CALLBACK', typeof (err) === 'object' ? JSON.stringify(err) : err);
+                    return undefined;
+                  });
+                  if (___response) {
+                    try {
+                      logger.debug('Success Request payload', typeof (_callbackResponse) === 'object' ? JSON.stringify(_callbackResponse) : _callbackResponse);
+                      logger.debug('SUCCESS ON CALLBACK', typeof (___response.data) === 'object' ? JSON.stringify(___response.data) : ___response.data);
+                    } catch (ex) {
+                      logger.debug('SUCCESS ON CALLBACK', "ERROR ON CALLBACK LOG with payload", JSON.stringify(_callbackResponse));
+                    }
+                  }
+
+                  dtoCopy.SentToCallback = true;
+                  await db.collection('responses').replaceOne({ DropId: dtoCopy.DropId }, dtoCopy, { upsert: true });
+                  await db.collection('responses_history').replaceOne({ DropId: dtoCopy.DropId }, dtoCopy, { upsert: true });
+                  await db.collection('responses').deleteOne({ DropId: dto.DropId })
+                  resolve();
+                } catch (err) {
+                  console.error('CALLBACK ERROR', err);
+                  process.send({ action: 'debug', message: err.stack });
+                  resolve();
+                }
+              });
+              tmrArr.push(pro);
+            }
+            try {
+              process.send({ action: 'debug', message: `Waiting for ${tmrArr.length} response records...` });
+              await Promise.allSettled(tmrArr);
+              process.send({ action: 'debug', message: `${tmrArr.length} Responses completed Sending...` });
+            } catch (err) {
+              process.send({ action: 'debug', message: err.stack });
+            }
+          }
+        }
+
+        this._callbackTimerObj = null;
+        this._callbackTimer();
+        return;
+      } catch (err) {
+        process.send({ action: 'debug', message: err.stack });
+        this._callbackTimerObj = null;
+        this._callbackTimer();
+        return;
+      }
+    }, 1000);
+  }
+
+  _respTimer() {
+    console.log('RESPTIMER STARTED');
+    if (this._respTimerObj) { return; }
+    this.respTimerObj = setTimeout(async () => {
+      try {
+        const tmrResps = await this._db.collection('responses').find({ SentToCallback: { $in: [null, false] }, callback_url: { $in: [null] } }).limit(250).toArray();
+        if (tmrResps.length) {
+          for (const tmr of tmrResps) {
+            try {
+              const dto = tmr;
+              const dtoCopy = Object.assign({}, dto);
+              delete dto._id;
+              delete dto.CallId;
+              delete dto.SentToCallback;
+              delete dto.DateCreated;
+              delete dto.StartDate;
+              delete dto.EndDate;
+              delete dto.Attempts;
+              delete dto.TotalSeconds;
+
+
+              dtoCopy.SentToCallback = true;
+              await this._db.collection('responses_history').replaceOne({ DropId: dtoCopy.Dropid }, dtoCopy, { upsert: true });
+              await this._db.collection('responses').deleteOne({ DropId: dto.DropId });
+              await this._db.collection('responses').findOneAndUpdate(
+                { DropId: dto.DropId, SentToCallback: { $in: [null, false] } },
+                { $set: { SentToCallback: true } }
+              );
+            } catch (err) {
+              process.send({ action: 'debug', message: err.stack });
+            }
+          }
+        }
+        this._respTimerObj = null;
+        this._respTimer();
+        return;
+      } catch (err) {
+        console.log('resp timer error ');
+        process.send({ action: 'debug', message: err.stack });
+        this._respTimerObj = null;
+        this._respTimer();
+        return;
+      }
+    }, 10 * 500);
+  }
+};
+
+module.exports = RespHandler;
+
+
--- server.js
+++ server.js
@@ -7,62 +7,88 @@ const passport = require("passport");
 const dotenv = require("dotenv");
 const compression = require("compression");
 const path = require('path');
-
+const log4js = require('log4js');
 const db_connect = require("./tools/db-connect");
 const { startCallsCronJob } = require("./crons");
+const RespHandler = require('./crons/callback/response');
 const { PUBLIC_FOLDER_NAME } = require("./global/constants");
+const cluster = require('cluster');
 
 
-const app = express();
-const SERVER_PORT = 3012;
-
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
-dotenv.config();
-
-db_connect
-  .DBConnectMongoose()
-  .then(() => {
-    const routes = require("./routes");
-    // passport for jwt authentication
-    app.use(passport.initialize());
-    require("./passport")(passport);
-
-    // bodyParser, parses the request body to be a readable json format
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json({ limit: "1000mb" }));
-    app.use(logger('combined', { stream: accessLogStream }));
-    
-
-    app.use(compression());
-    // app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.static(PUBLIC_FOLDER_NAME));
-
-    const whitelist = ["*"];
-    app.disable("x-powered-by");
-    app.options(whitelist, cors());
-    app.use(
-      cors({
-        credentials: true,
-        origin(origin, callback) {
-          callback(null, true);
-        },
-      })
-    );
-    app.get('/ui-logs-download', function(req, res){
-      const file = `${__dirname}/access.log`;
-      res.download(file); // Set disposition and send it.
-    });
-
-    app.get('/ccfuel-logs-download', function(req, res){
-      const file = `${__dirname}/public/debug.log`;
-      res.download(file); // Set disposition and send it.
-    });
-
-
-    routes.assignRoutes(app);
-    app.listen(SERVER_PORT);
-    console.log(`Server listening on port ` + SERVER_PORT);
-  })
-  .catch((err) => {
-    console.log("Error: " + err);
+if (cluster.isMaster) {
+  log4js.configure({
+    appenders: {
+      callback: {
+        type: 'multiFile', base: 'logs/', property: 'campaignId', extension: '.log'
+        // maxLogSize: 10485760, backups: 3, compress: true
+      }
+    },
+    categories: {
+      default: { appenders: ['callback'], level: 'debug' }
+    }
   });
+
+
+
+  const app = express();
+  const SERVER_PORT = 3012;
+
+  var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+  dotenv.config();
+
+  db_connect
+    .DBConnectMongoose()
+    .then(() => {
+      const routes = require("./routes");
+      // passport for jwt authentication
+      app.use(passport.initialize());
+      require("./passport")(passport);
+
+      // bodyParser, parses the request body to be a readable json format
+      app.use(bodyParser.urlencoded({ extended: false }));
+      app.use(bodyParser.json({ limit: "1000mb" }));
+      app.use(logger('combined', { stream: accessLogStream }));
+
+
+      app.use(compression());
+      // app.use(express.static(path.join(__dirname, 'public')));
+      app.use(express.static(PUBLIC_FOLDER_NAME));
+
+      const whitelist = ["*"];
+      app.disable("x-powered-by");
+      app.options(whitelist, cors());
+      app.use(
+        cors({
+          credentials: true,
+          origin(origin, callback) {
+            callback(null, true);
+          },
+        })
+      );
+      app.get('/ui-logs-download', function (req, res) {
+        const file = `${__dirname}/access.log`;
+        res.download(file); // Set disposition and send it.
+      });
+
+      app.get('/ccfuel-logs-download', function (req, res) {
+        const file = `${__dirname}/public/debug.log`;
+        res.download(file); // Set disposition and send it.
+      });
+
+
+      routes.assignRoutes(app);
+      app.listen(SERVER_PORT);
+      console.log(`Server listening on port ` + SERVER_PORT);
+      cluster.fork()
+
+    })
+    .catch((err) => {
+      console.log("Error: " + err);
+    });
+} else {
+  new RespHandler();
+
+}
+
+
+
